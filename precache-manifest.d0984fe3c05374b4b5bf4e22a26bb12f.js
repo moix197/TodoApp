f67367f149b12b82ad47aad2eba2f160f56fabef
Moises Montilla <cl_moises@hotmail.com>
2019-03-23 02:30:39 -0300

    Updates

---
+++ precache-manifest.d0984fe3c05374b4b5bf4e22a26bb12f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b429d889341aa25a2548",
+    "url": "/TodoApp/static/js/runtime~main.b429d889.js"
+  },
+  {
+    "revision": "5a3477e7a71a8141d9bb",
+    "url": "/TodoApp/static/js/main.5a3477e7.chunk.js"
+  },
+  {
+    "revision": "8ab38a4b211d48b2f97a",
+    "url": "/TodoApp/static/js/2.8ab38a4b.chunk.js"
+  },
+  {
+    "revision": "5a3477e7a71a8141d9bb",
+    "url": "/TodoApp/static/css/main.b154a910.chunk.css"
+  },
+  {
+    "revision": "1546cda56d06b49dbae99241e58d9b44",
+    "url": "/TodoApp/index.html"
+  }
+];
